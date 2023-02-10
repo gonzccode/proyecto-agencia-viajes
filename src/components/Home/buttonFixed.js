e7ce8bc726e3403b254e0142c7918983aaaff3cd
Federico Ramirez <ramirezsanabria.federico@gmail.com>
2023-02-10 00:36:25 -0300

    Más modificaciones

--- src/components/Home/buttonFixed.js
+++ src/components/Home/buttonFixed.js
@@ -5,7 +5,7 @@ import whatsappIcon from '../../assets/img/whatsapp-icon.png';
 const ButtonFixed = () => {
     return (
         <div id="buttonFixed">
-            <a  href="https://wa.me/+5492323533332" target="_blank">
+            <a  href="https://wa.me/+5492323533332" target="_blank" rel="noopener noreferrer">
                 <Button variant="primary" size="lg" className="writeUsButton"><Image src={whatsappIcon} alt="icono de whatsapp"/> ¡Escribinos!</Button>{' '}
             </a>
         </div>
